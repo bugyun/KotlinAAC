6728fb18b4cf3ac3d874e00cae3d8944d84abfb4
若云 <zyhdvlp@gmail.com>
2019-12-05 00:25:53 +0800

    修改代码

--- app/src/main/java/vip/ruoyun/googleaac/message/MessageActivity.java
+++ app/src/main/java/vip/ruoyun/googleaac/message/MessageActivity.java
@@ -4,46 +4,64 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Objects;
 
 import vip.ruoyun.googleaac.R;
 
 public class MessageActivity extends AppCompatActivity {
+
+    private static final String APP_SPECIFIC_SMS_RECEIVED_ACTION =
+            "com.android.cts.permission.sms.APP_SPECIFIC_SMS_RECEIVED";
+    private SMSBroadcastReceiver smsBroadcastReceiver;
+    private String appSpecificSmsToken;
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+        mTextView = findViewById(R.id.mTextView);
+        //注册广播接收者
+        smsBroadcastReceiver = new SMSBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(APP_SPECIFIC_SMS_RECEIVED_ACTION);
+        registerReceiver(smsBroadcastReceiver, filter);
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             SmsManager smsManager = SmsManager.getDefault();
-            String appSpecificSmsToken = smsManager.createAppSpecificSmsToken(createSmsTokenPendingIntent());
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(APP_SPECIFIC_SMS_RECEIVED_ACTION), PendingIntent.FLAG_ONE_SHOT);
+            appSpecificSmsToken = smsManager.createAppSpecificSmsToken(pendingIntent);
         }
 
-        for (SmsMessage pdu : Telephony.Sms.Intents.getMessagesFromIntent(getIntent())) {
-//            tv.append(pdu.getDisplayMessageBody());
-        }
 
     }
 
-    private PendingIntent createSmsTokenPendingIntent() {
-        Intent intent = new Intent(this, SMSBroadcastReceiver.class);
-        intent.setAction("com.lcw.alarm");
-        return PendingIntent.getBroadcast(this, 231, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(smsBroadcastReceiver);
     }
 
-    static class SMSBroadcastReceiver extends BroadcastReceiver {
+    private class SMSBroadcastReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (Objects.equals(intent.getAction(), "com.lcw.alarm")) {
-                //修改 ui
+            if (APP_SPECIFIC_SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+                for (SmsMessage pdu : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    String displayMessageBody = pdu.getDisplayMessageBody();
+                    if (displayMessageBody.contains(appSpecificSmsToken)) {
+                        //读取到短信密码
+                        mTextView.setText(displayMessageBody);
+                    }
+                }
             }
         }
     }
